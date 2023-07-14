da17d1b9b708995e27e21fb1c27a43b1c5cf49a0
Peemapol <aboutbeam@hotmail.com>
2023-07-15 00:51:34 +0700

    add async mode to socketio

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # to activate env write this in terminal (comand prompt) env\Scripts\activate.bat
 from flask import Flask, render_template, url_for, request, Response
 from flask_socketio import SocketIO, emit
-from flask_cors import CORS
 from asyncio import sleep
 import snscrape.modules.twitter as sntwitter
 from function.scrapeTweet import get_query
@@ -18,9 +17,9 @@ import json
 # from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-CORS(app)
+app.debug = True
 # app.config['SQLALCHEMY_DATABASE_URI']
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode='gevent')
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
